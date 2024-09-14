e09467f15bb777c97c3f8b2c9be67243e42cd34e
Isaac Silva <73505515+IsaacSilvaU@users.noreply.github.com>
2024-09-14 17:48:28 -0500

    test

--- models/producto.py
+++ models/producto.py
@@ -2,7 +2,7 @@
 from sqlalchemy import Column, Integer, String, Float
 from sqlalchemy.orm import relationship
 from .base_class import Base
-
+ 
 class Producto(Base):
     __tablename__ = "productos"
     id = Column(Integer, primary_key=True, index=True)
